0c399c40670da5d2a19f0602feddcd2dc36c8847
Anteneh Gedamu <gedamuanteneh@gmail.com>
2021-11-25 09:18:04 -0500

    count only function

--- tail.js
+++ tail.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,3 +7,13 @@ const assertEqual = function (actual, expected) {
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+const tail = function(arg1) {
+  if (arg1.length === 0) {
+    return "undefined";
+  }
+  return tail(arg1);
+};
+
+assertEqual(tail([5,6,7]), 5);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
