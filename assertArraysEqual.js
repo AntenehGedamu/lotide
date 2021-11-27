220116eee95a0d39cb0b01ef789c53f649c6fe06
Anteneh Gedamu <gedamuanteneh@gmail.com>
2021-11-27 08:54:02 -0500

    assert arrays function

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(arr1, arr2) {
+  if (!arr1.length === arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function () {
+  if (eqArrays() === true) {
+    console.log(`✅✅✅  Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
