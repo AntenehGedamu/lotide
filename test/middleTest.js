cb4fca5356722f323dfe376f1ebf94ca40031f15
Anteneh Gedamu <gedamuanteneh@gmail.com>
2021-12-03 08:17:28 -0500

    middle test js

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,7 +2,7 @@ const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
 
 
-console.log(middle([1]));
+console.log(assertArraysEqual(middle([1])));
 console.log(middle([1, 2]));
 console.log(middle([1, 2, 3]));
 console.log(middle([1, 2, 3, 4, 5]));
